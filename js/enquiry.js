e9bfc279bd24cfe5b68f5fd0401b88a66d78faf7
Sudhir Kumar <kumar.sudhir@orangemantra.in>
2026-04-10 16:43:03 +0530

    add icon

--- js/enquiry.js
+++ js/enquiry.js
@@ -274,8 +274,10 @@ function renderEnquiries() {
                             View Details
                         </a>
                         ${isAdmin() ? `
-                            <button onclick="deleteEnquiry('${enquiry._id || enquiry.id}')" class="text-red-600 hover:text-red-800 font-medium">
-                                Delete
+                            <button onclick="deleteEnquiry('${enquiry._id || enquiry.id}')" class="text-red-600 hover:text-red-800" title="Delete">
+                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                                </svg>
                             </button>
                         ` : ''}
                     </div>
